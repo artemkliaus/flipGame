68fa52411ed066561c28dac36e9f71828c01b1be
a.kliaus <a.kliaus@a.kliaus>
2018-10-16 23:55:46 +0300

    initial

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+//COMPONENTS
+import Field from './containers/Field';
+
+class App extends Component {
+    render() {
+        return (
+            <div className='App'>
+                <Field/>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/containers/Field.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../styles/Field.styl';
+
+class Field extends Component {
+
+    render () {
+        return (
+            <div className='field'>
+                Field is Here!
+            </div>
+        );
+    }
+}
+
+export default Field;

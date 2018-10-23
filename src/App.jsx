cc3c0be41f60c6f2fd5b03432099287169a42388
a.kliaus <a.kliaus@a.kliaus>
2018-10-23 18:21:26 +0300

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,26 @@ import './App.css';
 
 //COMPONENTS
 import Field from './containers/Field';
+import Popup from './components/Popup';
 
 class App extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            result: null
+        }
+    }
+
+    showResult (moves) {
+        const result = moves;
+        this.setState({result});
+    }
+
     render() {
         return (
             <div className='App'>
-                <Field/>
+                <Popup result={this.state.result}/>
+                <Field showResult={this.showResult.bind(this)}/>
             </div>
         );
     }

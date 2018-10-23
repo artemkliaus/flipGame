d56534e48ca1fea7256a5612bac41e06d45ba33a
a.kliaus <a.kliaus@a.kliaus>
2018-10-23 23:51:25 +0300

    make popup

--- src/App.jsx
+++ src/App.jsx
@@ -9,19 +9,42 @@ class App extends Component {
     constructor (props) {
         super(props);
         this.state = {
-            result: null
+            result: null,
+            popupView: 'start'
         }
     }
 
     showResult (moves) {
+        const popupView = this.state.popupView;
         const result = moves;
         this.setState({result});
+        this.setState({
+            popupView: 'end'
+        });
+    }
+
+    popupHandler (e) {
+        const button = e.target.id;
+        const popupView = this.state.popupView;
+        console.log(button);
+
+        if (button === 'start') {
+            this.setState({
+                popupView: 'hide'
+            })
+        }
+
+        if (button === 'rules') {
+            this.setState({
+                popupView: 'rules'
+            })
+        }
     }
 
     render() {
         return (
             <div className='App'>
-                <Popup result={this.state.result}/>
+                <Popup result={this.state.result} popupHandler={this.popupHandler.bind(this)} view={this.state.popupView}/>
                 <Field showResult={this.showResult.bind(this)}/>
             </div>
         );
--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -5,20 +5,35 @@ import '../styles/Popup.sass';
 
 function Popup (props) {
 
-    var divStyle = {
+    const divStyle = {
       backgroundImage: 'url(' + back + ')'
     };
 
-    return (
-        <div style={divStyle} className={ props.result ? 'popup popup__show' : 'popup' }>
-            <div className='popup__inner'>
-                <p className='popup__descr'>Найти двух одинаковых губернаторов: игра "Медузы"</p>
-                <div className='popup__result'>
-                    <p className='popup__result-text'>Я могу различить губернаторов с {props.result} раз</p>
+        if (props.view !== 'end') {
+            return (
+                <div style={divStyle} className={'popup popup_' + props.view}>
+                    <div className='popup__inner'>
+                        <p className='popup__descr'>Найти двух одинаковых губернаторов: игра "Медузы"</p>
+                        <div className='popup__result'>
+                            <button className='popup_start-button' onClick={(e) => props.popupHandler(e)} id='start'>Начать игру</button>
+                            <button className='popup_start-button' onClick={(e) => props.popupHandler(e)} id='rules'>Читать правила</button>
+                            <p className='popup_rules-text'>Поочередно переворачивая карты необходимо найти одинаковые за минимальное количество ходов</p>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    )
+            )
+        } else {
+            return (
+                <div style={divStyle} className={'popup popup_' + props.view}>
+                    <div className='popup__inner'>
+                        <p className='popup__descr'>Найти двух одинаковых губернаторов: игра "Медузы"</p>
+                        <div className='popup__result'>
+                            <p className='popup__result-text'>Я могу различить губернаторов с {props.result} раз</p>
+                        </div>
+                    </div>
+                </div>
+            )
+        }
 }
 
 export default Popup;
